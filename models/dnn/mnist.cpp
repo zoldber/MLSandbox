6878c14b3c0bc5191a3410340b9e4feea5e4111f
zoldber <zachary.oldberg@gmail.com>
2022-11-20 13:51:58 -0500

    Reformat dir structure

---
+++ models/dnn/mnist.cpp
@@ -0,0 +1,26 @@
+#include "read_idx.hpp"
+#include "../../nnet_toolkit/primitives.hpp"
+
+int main() {
+
+    /*
+    auto trainingImages = new idx::Set<unsigned char>("train-images.idx3-ubyte");
+    auto trainingLabels = new idx::Set<unsigned char>("train-labels.idx1-ubyte");
+
+    auto dims = trainingImages->dims();
+
+    uint32_t I, R, C;
+
+    I = std::get<0>(dims);
+    R = std::get<1>(dims);
+    C = std::get<2>(dims);
+
+    std::cout << I << ", " << R << ", " << C << std::endl; 
+    */
+
+   
+
+
+    return 0;
+
+}
---
+++ nnet_toolkit/primitives.hpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+namespace nnet {
+
+    template<class dataType>
+    class Layer {
+
+        private:
+
+        public:
+
+
+
+    };
+
+    template<class dataType>
+    class Network {
+
+        private:
+
+        public:
+
+            Network(std::vector dimensions);
+
+    };
+
+}
